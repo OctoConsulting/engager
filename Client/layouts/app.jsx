1219495b5f41d4c3efd949930de9c208dd5eb4fc
Andrew Tran <trandrew08@gmail.com>
2017-06-15 10:40:02 -0400

    Cleaned up

--- Client/layouts/app.jsx
+++
@@ -1,5 +0,0 @@
-window.ReactDOM= require("react-dom");
-window.React= require("react");
-window.Dashboard = require('./jsx/dashboard.jsx');
-window.NavBar = require('./jsx/nav.jsx');
-window.PerProfile = require('./jsx/perProfile.jsx');
