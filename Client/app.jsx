fa38edaf5de9dabf55286ffffcf9369c3a3c4c41
Andrew Tran <trandrew08@gmail.com>
2017-06-15 11:26:33 -0400

    Cleaning up master

--- Client/app.jsx
+++
@@ -1,7 +0,0 @@
-window.ReactDOM= require("react-dom");
-window.React= require("react");
-window.Dashboard = require('./jsx/dashboard.jsx');
-window.NavBar = require('./jsx/nav.jsx');
-window.PerProfile = require('./jsx/perProfile.jsx');
-window.Sign_In = require('./jsx/Sign_In.jsx');
-window.Sign_Up = require('./jsx/Sign_Up.jsx');
