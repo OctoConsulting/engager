e26f9cf475a8641c34096c216dd120f657d1640c
Andrew Tran <trandrew08@gmail.com>
2017-06-22 14:55:17 -0400

    Changed the backend stuff

---
+++ Server/user_creation.js
@@ -0,0 +1,15 @@
+import User from './user';
+import React, {Component} from 'react';
+
+
+class newUser extends Component {
+  constructor(props){
+    super(props);
+  }
+
+  let username, password;
+
+  this.props.map((fields) => {
+
+  });
+}
