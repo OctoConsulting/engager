316bda5ca41a919c266ff22a2cb5ba3019947e10
Andrew Tran <trandrew08@gmail.com>
2017-07-03 12:56:45 -0400

    Added endpoint for github integration

--- src/Server/services/GitHub.js
+++ src/Server/services/GitHub.js
@@ -2,25 +2,22 @@ const User = require('../models/user');
 const zlib = require('zlib');
 const request = require('request');
 
+
 module.exports = function(req, res, next){
   const user_id = req.params.id;
-  const stackoverflow_username = req.body.stackoverflow;
-
+  const github_username = req.body.github;
   request({
     method: 'GET',
-    uri: `https://api.stackexchange.com/2.2/users/${stackoverflow_username}/timeline?site=stackoverflow`,
-    gzip: true
+    uri: `https://api.github.com/users/${github_username}/events/public`
   }, function(error, response, body){
       const updated = {
-        username: stackoverflow_username,
-        data: JSON.parse(body)
+        username: github_username,
+        data: body
       }
 
-      User.findByIdAndUpdate({_id: user_id}, {$set: {stackoverflow : updated}})
+      User.findByIdAndUpdate({_id: user_id}, {$set: {github : updated}})
           .then(() => User.findById({_id: user_id}))
           .then( user => res.send(user))
           .catch(next);
   });
-
-
 }
