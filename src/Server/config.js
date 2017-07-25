afa65dfae30690175cacead93e0a8921eb424f73
Andrew Tran <trandrew08@gmail.com>
2017-07-25 14:46:52 -0400

    blanked out config.js

--- src/Server/config.js
+++ src/Server/config.js
@@ -4,15 +4,15 @@ module.exports = {
   secret: 'eriwer4309054erfhwiu32*&$#@d32',
   creation_timestamp: '1496667600', //June 5th, 2017 09:00AM EST
   Twitter : {
-    consumer_key: 'jlD8BSZCNsCqqDpmHgkelSB6A',
-    consumer_secret: 'zUEOxXBxfLJj3GmrWOS1DGCSZU9usa8htO5dSsgZIPXhSY7v3u',
-    bearer_token: 'AAAAAAAAAAAAAAAAAAAAAC9D1QAAAAAAi0AoObt49isSGovSBXoWR5uTpiY%3DVCKs7jmtj1mxSo9VQt18go9RRmdtknb0Nj3dTnLou1XTroxMoJ'
-},
+    consumer_key: '',
+    consumer_secret: '',
+    bearer_token: ''
+  },
   Instagram : {
-    id_key: 'bb65c1a4262349d78765097663077982',
-    id_secret: '2d472d1d92cc4802b91943e4edf107be',
-    access_url : 'https://api.instagram.com/oauth/authorize/?client_id=CLIENT-ID&redirect_uri=REDIRECT-URI&response_type=token'
-},
+    id_key: '',
+    id_secret: '',
+    access_url : ''
+  },
   EmailServer:{
     emailAccountUser: '',
     emailPassword: '',
