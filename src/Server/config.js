97be27d2540c9411275d1bc65acc8a2d1f4e2c6e
Andrew Tran <trandrew08@gmail.com>
2017-08-09 13:13:21 -0400

    blanking config.js

--- src/Server/config.js
+++ src/Server/config.js
@@ -4,9 +4,9 @@ module.exports = {
   secret: 'eriwer4309054erfhwiu32*&$#@d32',
   creation_timestamp: '1496667600', //June 5th, 2017 09:00AM EST
   Twitter : {
-    consumer_key: 'jlD8BSZCNsCqqDpmHgkelSB6A',
-    consumer_secret: 'zUEOxXBxfLJj3GmrWOS1DGCSZU9usa8htO5dSsgZIPXhSY7v3u',
-    bearer_token: 'AAAAAAAAAAAAAAAAAAAAAC9D1QAAAAAAi0AoObt49isSGovSBXoWR5uTpiY%3DVCKs7jmtj1mxSo9VQt18go9RRmdtknb0Nj3dTnLou1XTroxMoJ'
+    consumer_key: '',
+    consumer_secret: '',
+    bearer_token: ''
   },
   GitHub: {
     id_key: '',
