b9cf2377f33eb62aaa87d89919f096ed550c4010
Andrew Tran <trandrew08@gmail.com>
2017-08-12 16:48:42 -0400

    Added checks for null values to prevent server crashes

--- src/Server/services/instagram_handoff.js
+++ src/Server/services/instagram_handoff.js
@@ -32,32 +32,32 @@ module.exports = function(req, res, next){
 
   request(options, function(err, response, body){
     const data = JSON.parse(body).data;
-
-    let array = [];
-    let newDate;
-    //CONVERTS UTC TIME TO UNIX TIMESTAMP
-    //ONLY STORING TWEET IDs AND TIMESTAMPS
-    for (i=0; i < data.length; i++){
-      if (Number(data[i].created_time) >= Number(config.creation_timestamp)){
-        array[i] = {
-          date: Number(data[i].created_time),
-          id: data[i].id
+    if (data){
+      let array = [];
+      let newDate;
+      //CONVERTS UTC TIME TO UNIX TIMESTAMP
+      //ONLY STORING TWEET IDs AND TIMESTAMPS
+      for (i=0; i < data.length; i++){
+        if (Number(data[i].created_time) >= Number(config.creation_timestamp)){
+          array[i] = {
+            date: Number(data[i].created_time),
+            id: data[i].id
+          }
         }
       }
+
+      const updated = {
+        username: instagram_username,
+        data: array,
+        actions: array.length,
+        points: array.length * 1.5
+      };
+
+      User.findByIdAndUpdate({_id: user_id}, {$set: {instagram: updated}})
+          .then(() => User.findById({_id: user_id}))
+          .then( user => res.send(html_response_string))
+          .catch(next);
+          // twitter_data = data;
     }
-
-    const updated = {
-      username: instagram_username,
-      data: array,
-      actions: array.length,
-      points: array.length * 1.5
-    };
-
-    User.findByIdAndUpdate({_id: user_id}, {$set: {instagram: updated}})
-        .then(() => User.findById({_id: user_id}))
-        .then( user => res.send(html_response_string))
-        .catch(next);
-        // twitter_data = data;
-
   });
 }
