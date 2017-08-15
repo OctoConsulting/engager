af1e4d8346b4b1019666b2d9b52e26789bbc5da0
Andrew Tran <trandrew08@gmail.com>
2017-08-15 15:05:18 -0400

    Added some styling for public profile

--- src/Client/components/Dashboard/Form_Public_Modal.js
+++ src/Client/components/Dashboard/Form_Public_Modal.js
@@ -46,7 +46,115 @@ class Form_Public_Modal extends Component {
     return formatted;
   }
 
+
   render(){
+    const instagram_connect = (this.state.userInfo !== null && this.state.userInfo.instagram.username !== '') ? <span className="col-md-4 col-sm-2 col-xs-2 inner-all no-margin">
+      <div className="panel rounded shadow">
+        <div className="panel-heading text-center bg-instagram">
+          <p className="inner-all no-margin">
+            <i className="fa fa-instagram fa-5x"></i>
+          </p>
+        </div>
+        <div className="panel-body text-center">
+          <strong> {(this.state.userInfo !== null) ? this.state.userInfo.instagram.username : ''} </strong> <br />
+          actions: {(this.state.userInfo !== null) ? this.state.userInfo.instagram.actions : ''} <br />
+          points: {(this.state.userInfo !== null) ? this.state.userInfo.instagram.points : ''}
+        </div>
+      </div>
+    </span> : null;
+
+    const twitter_connect = (this.state.userInfo !== null && this.state.userInfo.twitter.username !== '') ? <span className="col-md-4 col-sm-2 col-xs-2 inner-all no-margin">
+      <div className="panel rounded shadow">
+        <div className="panel-heading text-center bg-twitter">
+          <p className="inner-all no-margin">
+            <i className="fa fa-twitter fa-5x"></i>
+          </p>
+        </div>
+        <div className="panel-body text-center">
+          <strong> {(this.state.userInfo !== null) ? this.state.userInfo.twitter.username : ''} </strong> <br />
+          actions: {(this.state.userInfo !== null) ? this.state.userInfo.twitter.actions : ''} <br />
+          points: {(this.state.userInfo !== null) ? this.state.userInfo.twitter.points : ''}
+        </div>
+      </div>
+    </span> : null;
+
+    const stackoverflow_connect = (this.state.userInfo !== null && this.state.userInfo.stackoverflow.username !== '') ? <span className="col-md-4 col-sm-2 col-xs-2 inner-all no-margin">
+      <div className="panel rounded shadow">
+        <div className="panel-heading text-center bg-stackOverFlow">
+          <p className="inner-all no-margin">
+            <i className="fa fa-stack-overflow fa-5x"></i>
+          </p>
+        </div>
+        <div className="panel-body text-center">
+          <strong> {(this.state.userInfo !== null) ? this.state.userInfo.stackoverflow.username : ''} </strong> <br />
+          actions: {(this.state.userInfo !== null) ? this.state.userInfo.stackoverflow.actions : ''} <br />
+          points: {(this.state.userInfo !== null) ? this.state.userInfo.stackoverflow.points : ''}
+        </div>
+      </div>
+    </span> : null;
+
+    const jsfiddle_connect = (this.state.userInfo !== null && this.state.userInfo.jsfiddle.username !== '') ? <span className="col-md-4 col-sm-2 col-xs-2 inner-all no-margin">
+      <div className="panel rounded shadow">
+        <div className="panel-heading text-center bg-jsFiddle">
+          <p className="inner-all no-margin">
+            <i className="fa fa-jsfiddle fa-5x"></i>
+          </p>
+        </div>
+        <div className="panel-body text-center">
+          <strong> {(this.state.userInfo !== null) ? this.state.userInfo.jsfiddle.username : ''} </strong> <br />
+          actions: {(this.state.userInfo !== null) ? this.state.userInfo.jsfiddle.actions : ''} <br />
+          points: {(this.state.userInfo !== null) ? this.state.userInfo.jsfiddle.points : ''}
+        </div>
+      </div>
+    </span> : null;
+
+
+    const facebook_connect = (this.state.userInfo !== null && this.state.userInfo.facebook.username !== '') ? <span className="col-md-4 col-sm-2 col-xs-2 inner-all no-margin">
+      <div className="panel rounded shadow">
+        <div className="panel-heading text-center bg-facebook">
+          <p className="inner-all no-margin">
+            <i className="fa fa-facebook fa-5x"></i>
+          </p>
+        </div>
+        <div className="panel-body text-center">
+          <strong> {(this.state.userInfo !== null) ? this.state.userInfo.facebook.username : ''} </strong> <br />
+          actions: {(this.state.userInfo !== null) ? this.state.userInfo.facebook.actions : ''} <br />
+          points: {(this.state.userInfo !== null) ? this.state.userInfo.facebook.points : ''}
+        </div>
+      </div>
+    </span> : null;
+
+    const github_connect = (this.state.userInfo !== null && this.state.userInfo.github.username !== '') ? <span className="col-md-4 col-sm-2 col-xs-2 inner-all no-margin">
+      <div className="panel rounded shadow">
+        <div className="panel-heading text-center bg-github">
+          <p className="inner-all no-margin">
+            <i className="fa fa-github fa-5x"></i>
+          </p>
+        </div>
+        <div className="panel-body text-center">
+          <strong> {(this.state.userInfo !== null) ? this.state.userInfo.github.username : ''} </strong> <br />
+          actions: {(this.state.userInfo !== null) ? this.state.userInfo.github.actions : ''} <br />
+          points: {(this.state.userInfo !== null) ? this.state.userInfo.github.points : ''}
+        </div>
+      </div>
+    </span> : null;
+
+    const linkedin_connect = (this.state.userInfo !== null && this.state.userInfo.linkedin.username !== '') ? <span className="col-md-4 col-sm-2 col-xs-2 inner-all no-margin">
+      <div className="panel rounded shadow">
+        <div className="panel-heading text-center bg-linkedin">
+          <p className="inner-all no-margin">
+            <i className="fa fa-linkedin fa-5x"></i>
+          </p>
+        </div>
+        <div className="panel-body text-center">
+          <strong> {(this.state.userInfo !== null) ? this.state.userInfo.linkedin.username : ''} </strong> <br />
+          actions: {(this.state.userInfo !== null) ? this.state.userInfo.linkedin.actions : ''} <br />
+          points: {(this.state.userInfo !== null) ? this.state.userInfo.linkedin.points : ''}
+        </div>
+      </div>
+    </span> : null;
+
+    const empty_message = (this.state.userInfo !== null && (this.state.userInfo.profile.points - this.state.userInfo.events.points) == 0) ? <div id="for"><p className="fa fa-chain-broken fa-5x"></p><br/><strong>This user does not have any integration connected. Please check back later.</strong></div> : null;
     return(
       <form >
         <div className="modal fade" id="publicProfile" role="dialog">
@@ -55,130 +163,27 @@ class Form_Public_Modal extends Component {
             }
             <div className="modal-content">
               <div className="modal-header" id="submission-form">
-                <h4 className="modal-title">{(this.state.userInfo !== null) ? this.state.userInfo.name : ''}</h4>
+                <strong><h4 className="modal-title">{(this.state.userInfo !== null) ? this.state.userInfo.name : ''}</h4></strong>
               </div>
 
 
               <div className="modal-body">
                 <div className="row text-center">
-
-
-
-                  <span className="col-md-4 col-sm-2 col-xs-2 inner-all no-margin">
-                    <div className="panel rounded shadow">
-                      <div className="panel-heading text-center bg-instagram">
-                        <p className="inner-all no-margin">
-                          <i className="fa fa-instagram fa-5x"></i>
-                        </p>
-                      </div>
-                      <div className="panel-body text-center">
-                        <strong> {(this.state.userInfo !== null) ? this.state.userInfo.instagram.username : ''} </strong> <br />
-                        actions: {(this.state.userInfo !== null) ? this.state.userInfo.instagram.actions : ''} <br />
-                        points: {(this.state.userInfo !== null) ? this.state.userInfo.instagram.points : ''}
-                      </div>
-                    </div>
-                  </span>
-
-
-                  <span className="col-md-4 col-sm-2 col-xs-2 inner-all no-margin">
-                    <div className="panel rounded shadow">
-                      <div className="panel-heading text-center bg-twitter">
-                        <p className="inner-all no-margin">
-                          <i className="fa fa-twitter fa-5x"></i>
-                        </p>
-                      </div>
-                      <div className="panel-body text-center">
-                        <strong> {(this.state.userInfo !== null) ? this.state.userInfo.twitter.username : ''} </strong> <br />
-                        actions: {(this.state.userInfo !== null) ? this.state.userInfo.twitter.actions : ''} <br />
-                        points: {(this.state.userInfo !== null) ? this.state.userInfo.twitter.points : ''}
-                      </div>
-                    </div>
-                  </span>
-
-
-                  <span className="col-md-4 col-sm-2 col-xs-2 inner-all no-margin">
-                    <div className="panel rounded shadow">
-                      <div className="panel-heading text-center bg-stackOverFlow">
-                        <p className="inner-all no-margin">
-                          <i className="fa fa-stack-overflow fa-5x"></i>
-                        </p>
-                      </div>
-                      <div className="panel-body text-center">
-                        <strong> {(this.state.userInfo !== null) ? this.state.userInfo.stackoverflow.username : ''} </strong> <br />
-                        actions: {(this.state.userInfo !== null) ? this.state.userInfo.stackoverflow.actions : ''} <br />
-                        points: {(this.state.userInfo !== null) ? this.state.userInfo.stackoverflow.points : ''}
-                      </div>
-                    </div>
-                  </span>
-
-
-                  <span className="col-md-4 col-sm-2 col-xs-2 inner-all no-margin">
-                    <div className="panel rounded shadow">
-                      <div className="panel-heading text-center bg-jsFiddle">
-                        <p className="inner-all no-margin">
-                          <i className="fa fa-jsfiddle fa-5x"></i>
-                        </p>
-                      </div>
-                      <div className="panel-body text-center">
-                        <strong> {(this.state.userInfo !== null) ? this.state.userInfo.jsfiddle.username : ''} </strong> <br />
-                        actions: {(this.state.userInfo !== null) ? this.state.userInfo.jsfiddle.actions : ''} <br />
-                        points: {(this.state.userInfo !== null) ? this.state.userInfo.jsfiddle.points : ''}
-                      </div>
-                    </div>
-                  </span>
-
-
-                  <span className="col-md-4 col-sm-2 col-xs-2 inner-all no-margin">
-                    <div className="panel rounded shadow">
-                      <div className="panel-heading text-center bg-facebook">
-                        <p className="inner-all no-margin">
-                          <i className="fa fa-facebook fa-5x"></i>
-                        </p>
-                      </div>
-                      <div className="panel-body text-center">
-                        <strong> {(this.state.userInfo !== null) ? this.state.userInfo.facebook.username : ''} </strong> <br />
-                        actions: {(this.state.userInfo !== null) ? this.state.userInfo.facebook.actions : ''} <br />
-                        points: {(this.state.userInfo !== null) ? this.state.userInfo.facebook.points : ''}
-                      </div>
-                    </div>
-                  </span>
-
-
-                  <span className="col-md-4 col-sm-2 col-xs-2 inner-all no-margin">
-                    <div className="panel rounded shadow">
-                      <div className="panel-heading text-center bg-github">
-                        <p className="inner-all no-margin">
-                          <i className="fa fa-github fa-5x"></i>
-                        </p>
-                      </div>
-                      <div className="panel-body text-center">
-                        <strong> {(this.state.userInfo !== null) ? this.state.userInfo.github.username : ''} </strong> <br />
-                        actions: {(this.state.userInfo !== null) ? this.state.userInfo.github.actions : ''} <br />
-                        points: {(this.state.userInfo !== null) ? this.state.userInfo.github.points : ''}
-                      </div>
-                    </div>
-                  </span>
-
-                  <span className="col-md-4 col-sm-2 col-xs-2 inner-all no-margin">
-                    <div className="panel rounded shadow">
-                      <div className="panel-heading text-center bg-linkedin">
-                        <p className="inner-all no-margin">
-                          <i className="fa fa-linkedin fa-5x"></i>
-                        </p>
-                      </div>
-                      <div className="panel-body text-center">
-                        <strong> {(this.state.userInfo !== null) ? this.state.userInfo.linkedin.username : ''} </strong> <br />
-                        actions: {(this.state.userInfo !== null) ? this.state.userInfo.linkedin.actions : ''} <br />
-                        points: {(this.state.userInfo !== null) ? this.state.userInfo.linkedin.points : ''}
-                      </div>
-                    </div>
-                  </span>
-
-
+                  {empty_message}
+                  {instagram_connect}
+                  {twitter_connect}
+                  {stackoverflow_connect}
+                  {jsfiddle_connect}
+                  {facebook_connect}
+                  {github_connect}
+                  {linkedin_connect}
+                  <br/>
                 </div>
-
+                <hr/>
                 <div className="row">
                   <div className="col-md-12 col-sm-12 col-xs-12">
+                    <h4 id="event-title">Event Information</h4>
+                    <br/>
                     <BootstrapTable data={(this.state.userInfo !== null) ? this.state.userInfo.events.data : null} search={true} pagination bordered>
                       <TableHeaderColumn dataField='date' dataSort={true} isKey={true} dataFormat={this.dateFormatter}>Date</TableHeaderColumn>
                       <TableHeaderColumn dataField='type' dataSort={true}>Type</TableHeaderColumn>
